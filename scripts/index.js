643224fa13357c54d1acf755fd3a68d2bcf4303d
Anastasia Stroganova <nastasiyaulich@yandex.ru>
2025-06-13 16:56:25 +0300

    final

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+initialCards.forEach((item) => {
+  addToContainer(cardsList, addCard(item, deleteCard));
+});
 
-// @todo: Функция создания карточки
+function addToContainer (cardsList, cardElement) {
+  cardsList.append(cardElement);
+}
 
-// @todo: Функция удаления карточки
+function addCard(cardData, deleteCardCallback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+
+  deleteButton.addEventListener('click', () => {
+    deleteCardCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
